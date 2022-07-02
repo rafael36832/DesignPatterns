9b639132e770fd0f27a03c86b13260c8dee2b5a5
rafael36832 <rafael.brum@ufv.br>
2022-07-02 12:50:30 -0300

    Abstract Factory

--- src/creational/abstractFactory/client-code.ts
+++ src/creational/abstractFactory/client-code.ts
@@ -1,7 +1,12 @@
 import { IndividualVehicleCustomerFactory } from "./factories/IndividualVehicleCustomerFactory";
-
+import { EnterpriseVehicleCustomerFactory } from "./factories/EnterpriseVehicleCustomerFactory";
 const vehicle1 = new IndividualVehicleCustomerFactory().createVehicle(
   "Ethios",
   "Rose",
 );
+const vehicle2 = new EnterpriseVehicleCustomerFactory().createVehicle(
+  "Ferrari",
+  "Rubens",
+);
 vehicle1.pickUp();
+vehicle2.pickUp();
